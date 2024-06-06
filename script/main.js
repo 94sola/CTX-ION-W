bbf26e5b0786dc2da146e87b6a80e709a1721b3e
A-believer <davidabolade29@gmail.com>
2024-06-06 06:57:42 +0100

    all done

--- script/main.js
+++ script/main.js
@@ -1,1 +1,26 @@
+ const list = document.getElementById('mobile')
+    const openBtn = document.getElementById('open')
+    const closeBtn = document.getElementById('close')
 
+openBtn.addEventListener("click", () => {
+    list.classList.remove('hidden')
+    list.classList.add('flex')
+    
+    openBtn.classList.remove('flex')
+    openBtn.classList.add('hidden')
+    closeBtn.classList.remove('hidden')
+    closeBtn.classList.add('flex')
+      console.log('open works')
+    })
+
+closeBtn.addEventListener("click", () => {
+        list.classList.remove('flex')
+        list.classList.add('hidden')
+
+        closeBtn.classList.add('hidden')
+        closeBtn.classList.remove('flex')
+        openBtn.classList.remove('hidden')
+    openBtn.classList.add('flex')
+      console.log('close works')
+
+    })
